c58b68919fbab0a5184fcaf1306d0b669598e82e
Henrique Jardim <henriquefjardim@gmail.com>
2017-09-19 12:08:08 -0300

    Início de algumas mecânicas com placeholders
    O personagem anda e atira.

---
+++ Em Busca do Pai/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float Speed = 5f;
+	public GameObject Bullet;
+
+	private Vector2 m_Movement;
+	private Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		m_Movement = new Vector2 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"));
+
+		m_Movement *= Speed * Time.deltaTime;
+
+		rb.velocity = m_Movement;
+
+		if (Input.GetKeyDown(KeyCode.Space))
+			Shoot ();
+
+	}
+
+	void Shoot(){
+		Instantiate (Bullet, transform.position, transform.rotation);
+	}
+}
---
+++ Em Busca do Pai/Assets/Scripts/ProjectileController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileController : MonoBehaviour {
+
+
+	public float speed = 200f;
+
+ 
+   
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (-1 * (Vector2.down * speed * Time.deltaTime)); 
+	}
+}
